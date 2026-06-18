deb03ebc7ced9c4633b97fb335a2b5c2da5ca007
okuma-space <narihirookuma@gmail.com>
2026-06-18 22:25:45 +0900

    add linear motion dynamics

---
+++ docs/report.md
@@ -0,0 +1,31 @@
+
+# シミュレーション自習結果サマリ_v2.0
+[previous version](https://github.com/okuma-space/balloon-simulation/blob/main/docs/reports/report_v1.0.md)
+
+## 1.気球飛翔ダイナミクスシミュレーション
+
+## 2 運動ダイナミクスモデル
+
+## 3 気球モデル
+
+## 4 制御モデル
+
+## 5 Environment models(環境モデル)
+
+## Appendix. 過去versionの検証ログ(保存/振り返り用)
+### version0.1
+version0.1として一次元の直線運動のダイナミクスを実装.
+
+[Repository](https://github.com/okuma-space/balloon-simulation/tree/v1.3)
+
+[PR](https://github.com/okuma-space/balloon-simulation/pull/47)
+
+初期値0でとりあえず横一直線グラフの作成ができていることが確認できた.
+
+###### Figures 
+![trajectry](https://okuma-space.github.io/control-and-dynamics-sim/images/generated/v0/linear_motion_result_0.0_)
+
+
+___
+___
+
---
+++ src/simulate.py
@@ -0,0 +1,47 @@
+import dynamics
+import numpy as np
+import visualize
+
+
+def simulate():
+    """
+    Simulates the linear motion of a system using the linear motion dynamics model.
+    """
+
+    # 初期値
+    simulate_time = 10.0  # シミュレーション実行時間[sec]
+    resolution_sec = 0.01  # シミュレーション時間解像度[sec]
+    current_state = np.array([0.0, 0.0])  # 初期位置[m],初期速度[m/s]
+
+    # 型準備
+    time_vector = []
+    current_time = 0.0
+    state_history = []
+
+    # 伝搬ループ
+    while current_time < simulate_time:
+        # ダイナミクスモデルから状態ベクトルの一次微分を取得する
+        state_derivative = dynamics.linear_motion_model(
+            current_state, np.array([0.0])
+        )  # 初期加速度[m/s^2]
+
+        # 状態ベクトルを更新する
+        current_state = current_state + state_derivative * resolution_sec
+
+        # 状態ベクトルを保存する
+        state_history.append(current_state.copy())
+
+        # 時刻の更新
+        time_vector.append(current_time)
+        current_time += resolution_sec
+
+    # visualize
+    visualize.plot_linear_motion(
+        state_history,
+        time_vector,
+        output_path="docs/linear_motion_result.png",
+    )
+
+
+if __name__ == "__main__":
+    simulate()
